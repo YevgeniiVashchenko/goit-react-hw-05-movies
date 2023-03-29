ab5d22e1120ac2613e129170b3c8464cebbe368d
YevgeniiVashchenko <yevgenii.vashchenko@gmail.com>
2023-03-29 16:34:27 +0300

    fix goBack

--- src/components/BackLink/BackLink.jsx
+++ src/components/BackLink/BackLink.jsx
@@ -1,11 +1,13 @@
 import { useLocation } from 'react-router-dom';
+import { useRef } from 'react';
 import { MdOutlineKeyboardBackspace } from 'react-icons/md';
 import { GoBack } from './BackLink.styled';
 
 const BackLink = () => {
   const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/');
   return (
-    <GoBack to={location?.state?.from ?? '/'}>
+    <GoBack to={backLinkLocationRef.current}>
       <MdOutlineKeyboardBackspace />
       Go back
     </GoBack>
